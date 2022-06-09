228865c15fde4a1b372280b48a53f93fe88dbc6d
Daniel316023386SC <93101052+Daniel316023386SC@users.noreply.github.com>
2022-06-09 13:11:09 -0500

    Update encabezado.php

--- CRUD NUEVO/encabezado.php
+++ CRUD NUEVO/encabezado.php
@@ -12,7 +12,7 @@ CRUD con PostgreSQL y PHP
     <link href="style.css" rel="stylesheet">
 </head>
 <body>
-    <!-- Definición del menú -->    <-- Hola mundo departe de Daniel -->
+    <!-- Definición del menú -->   
     <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <a class="navbar-brand" target="_blank">
       CRUD
